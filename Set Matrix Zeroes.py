5c1bcc2e2b78cc58a8e0548a0d566ba125fb353a
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-06-23 19:59:07 +0530

    Create Set Matrix Zeroes.py
    https://www.codingninjas.com/studio/problems/set-matrix-zeros_8230862?challengeSlug=striver-sde-challenge&leftPanelTab=1

---
+++ Set Matrix Zeroes.py
@@ -0,0 +1,21 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+from typing import List
+
+def setZeros(mat: List[List[int]]) -> None:
+    n, m = len(mat), len(mat[0])
+    row, col = {}, {}
+    for i in range(n) :
+        for j in range(m) :
+            if mat[i][j] == 0:
+                row[i] = 1
+                col[j] = 1
+    for i in row:
+        for j in range(m) :
+            mat[i][j] = 0
+    for j in col:
+        for i in set(range(n)) - row.keys() :
+            mat[i][j] = 0
