6ec4db96e1ae918f0f89b53e6275efd8caf2bf9f
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-07-15 22:30:08 +0530

    day 21

---
+++ 70. kthSmallLarge.py
@@ -0,0 +1,11 @@
+import heapq
+
+def kthSmallLarge(arr, n, k):
+    heapq.heapify(arr)
+    while len(arr) >= min(k, n-k+1):
+        top = heapq.heappop(arr)
+        if len(arr)+1 == n - k + 1:
+            kthSmallest = top
+        if len(arr)+1 == k:
+            kthLargest = top
+    return kthSmallest, kthLargest
