a69502f59bf28716c2cca6df539a02ade981264a
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-07 22:59:18 +0530

    day 45

---
+++ 138. findTarget.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    set<int>st;
+    bool findTarget(TreeNode* root, int k) {
+        if(root==NULL) return false;
+        if(st.find(k-root->val)!=st.end()) return true;
+        else{
+            st.insert(root->val);
+        }
+        return (findTarget(root->left,k) || findTarget(root->right,k));
+    }
+};
