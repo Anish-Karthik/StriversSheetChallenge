dde2f2fcd7e8fd128ee3787e44a08180bfbd034d
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-07 22:57:55 +0530

    day 44

---
+++ 137. kthLargest.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void Inorder(TreeNode* root, vector<int>&vec){
+        if(root == NULL)
+        return;
+      Inorder(root->left,vec);
+      vec.push_back(root->val);
+      Inorder(root->right,vec);
+   }
+    int kthLargest(TreeNode* root, int k) {
+        vector<int>vec;
+        Inorder(root,vec);
+        return vec[n-k];
+    }
+};
