8fc857b8b1a7a37eb26fc9f34d94d6506159333c
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-08 21:16:31 +0530

    day 45

---
+++ 142. flatten.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        
+        if(root==NULL)
+            return;
+        TreeNode* temp=root->right;
+        
+        root->right=root->left;
+        root->left=NULL;
+
+        TreeNode* rightMost=root;
+        while(rightMost->right)
+        {
+            rightMost=rightMost->right;
+        }
+
+        rightMost->right=temp;
+
+        root=root->right;
+        flatten(root);
+    }
+};
