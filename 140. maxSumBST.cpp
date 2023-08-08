2fb044b253f8cf368f30af1569f90d60b7c352fa
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-08 21:13:51 +0530

    day 46

---
+++ 140. maxSumBST.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class nodeValue{
+     public:
+     int min;
+     int max;
+     int sum;
+     nodeValue(int min,int max,int sum)
+     {
+         this->min=min;
+         this->max=max;
+         this->sum=sum;
+     }
+ };
+class Solution {
+public:
+    int ans=0;
+    int maxSumBST(TreeNode* root) {
+        fun(root);
+        return ans;
+    }
+    nodeValue fun(TreeNode* root)
+    {
+        if(root==NULL)
+        return nodeValue(INT_MAX,INT_MIN,0);
+        nodeValue l=fun(root->left);
+        nodeValue r=fun(root->right);
+        if(root->val>l.max && root->val<r.min)
+        {
+            ans=max(ans,root->val+l.sum+r.sum);
+            return nodeValue(min(root->val,l.min),max(root->val,r.max),l.sum+root->val+r.sum);
+        }
+        return nodeValue(INT_MIN,INT_MAX,max(l.sum,r.sum));
+    }
+
+};
