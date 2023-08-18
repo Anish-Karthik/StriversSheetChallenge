a27b7c9c36626dc64b225c29168cb142b9766d18
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-18 21:37:07 +0530

    day 55

---
+++ 170. Edit Distance.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+     int minDistance(string word1, string word2) {
+    
+        int l1 = word1.size();
+        int l2 = word2.size();
+    
+        vector<int> f(l2+1, 0);
+        for (int j = 1; j <= l2; ++j)
+            f[j] = j;
+    
+        for (int i = 1; i <= l1; ++i)
+        {
+            int prev = i;
+            for (int j = 1; j <= l2; ++j)
+            {
+                int cur;
+                if (word1[i-1] == word2[j-1]) {
+                    cur = f[j-1];
+                } else {
+                    cur = min(min(f[j-1], prev), f[j]) + 1;
+                }
+    
+                f[j-1] = prev;
+                prev = cur;
+            }
+            f[l2] = prev;
+        }
+        return f[l2];
+    
+    }  
+};
